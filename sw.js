6d4c87bf67e74e2a3b484ace64b12f7ef04ebb93
uveli256-hash <uveli256@gmail.com>
2026-03-30 16:01:38 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE = 'trafik-v1';
+const CACHE = 'trafik-v6';
 const FILES = ['./index.html', './manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); });
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
